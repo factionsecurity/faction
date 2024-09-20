20403efe4f5642ff0cdf71138639a85e4ecb0832
Josh <ascetik@gmail.com>
2024-09-19 20:15:36 -0500

    mongo ssl option

--- src/com/fuse/dao/HibHelper.java
+++ src/com/fuse/dao/HibHelper.java
@@ -43,7 +43,10 @@ public class HibHelper {
 				properties.put("hibernate.ogm.datastore.database", System.getenv("FACTION_MONGO_DATABASE"));
 				properties.put("hibernate.ogm.datastore.port", System.getenv("FACTION_MONGO_PORT"));
 				//if(!System.getenv("FACTION_MONGO_AUTH_DATABASE").equals(""))
-				properties.put("hibernate.ogm.mongodb.authentication_database", System.getenv("FACTION_MONGO_AUTH_DATABASE"));
+				properties.put("hibernate.ogm.mongodb.retryWrites", "false");
+				properties.put("hibernate.ogm.mongodb.connectTimeoutMS", "3000");
+				properties.put("hibernate.ogm.mongodb.readPreference", "secondaryPreferred");
+				properties.put("hibernate.ogm.mongodb.replicaSet", "rs");
 				if(System.getenv("FACTION_MONGO_SSL").equals("true")) {
 					properties.put("hibernate.ogm.mongodb.driver.ssl.enabled", "true");
 				}
